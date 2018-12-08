f436b8205ba270131d98d50fa5f7e8348aed3713
Omar Diab <od@eco.com>
2018-12-07 18:52:11 -0800

    add installing ios 10 14 headers for pyenv

--- zsh/packages.sh
+++ zsh/packages.sh
@@ -10,6 +10,9 @@ brew install\
 		nvm\
 		httpie
 
+echo ">> installing Mac OS SDK headers for iOS 10.14 (Mojave)"
+sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+
 echo ">> installing ruby version manager (rvm)\n"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
