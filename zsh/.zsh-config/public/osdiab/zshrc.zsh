791c1886894a855f8b2876bc79482e9b84e0b448
Omar Diab <od@eco.com>
2018-12-31 01:54:10 -0500

    react-native config

--- zsh/.zsh-config/public/osdiab/zshrc.zsh
+++ zsh/.zsh-config/public/osdiab/zshrc.zsh
@@ -68,7 +68,9 @@ source "$ZSH_CONFIG/public/osdiab/utilities.zsh"
 # Add stuff to path #
 #####################
 
-export ANDROID_HOME=${HOME}/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$GOPATH/bin
