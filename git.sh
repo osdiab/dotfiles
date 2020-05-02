99e9ccea30b9875754152de5a2fa661a56ba5ba5
Omar Diab <me@omardiab.com>
2020-05-02 16:35:25 +0900

    Gitcommands

---
+++ git.sh
@@ -0,0 +1,10 @@
+# Git
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.pushup 'push -u origin HEAD'
+git config --global alias.newbranch 'checkout -b'
+# End git
+
