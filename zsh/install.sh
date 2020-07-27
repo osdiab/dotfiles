4ceafec367c77ec509db38323dba34612bc7b66e
Omar Diab <me@omardiab.com>
2020-07-27 14:24:47 +0900

    sync icloud drive with personal.zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,14 @@
 (
   SCRIPT=$(realpath "$0")
-  # Absolute path this script is in, thus /home/user/bin
+  # Absolute path this script is in
   SCRIPTPATH=$(dirname "$SCRIPT")
 
+  # this path might change in newer versions of Mac OSX
+  ln -s\
+    "$HOME/Library/Mobile Documents/com~apple~CloudDocs/config/zsh/personal.zsh"\
+    "$HOME/.config/zsh/personal.zsh"
   ln -s $SCRIPTPATH "$HOME/.config/zsh"
   echo 'source "$HOME/.config/zsh/.zshrc"' > $HOME/.zshrc
+
+  echo 'Please ensure personal iCloud is synced.'
 )
