ff423a4bc43de45c08d8dc88f06e78acfc13088a
Omar Diab <me@omardiab.com>
2020-07-27 14:24:57 +0900

    include keychain, ssh stuff in git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,25 @@
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+  echo "================"
+  echo "Creating SSH key"
+  echo "================"
+  ssh-keygen
+  echo "\n\n"
+fi
+
+echo "==========================="
+echo "Adding SSH keys to keychain"
+echo "==========================="
+
+ssh-add -K
+
+echo "\n\n"
+
+echo "==============="
+echo "Configuring git"
+echo "==============="
+
 # Git
+git config --global credential.helper osxkeychain
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
