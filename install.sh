883408def14476d063e2fc8638d2f5438998a2bf
Omar Diab <me@omardiab.com>
2020-07-27 12:52:04 +0900

    status for install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-# install homebrew
+echo "=================="
+echo "Installing brew..."
+echo "=================="
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install casks
 
+echo "==================="
+echo "Installing casks..."
+echo "==================="
 brew cask install\
 	1password\
 	disk-inventory-x\
@@ -49,7 +54,11 @@ brew cask install\
 	native-access\
 	spotify
 
-# install cli tools
+echo "\n\n"
+
+echo "======================="
+echo "Installing cli tools..."
+echo "======================="
 brew tap klaaspieter/formula
 brew tap heroku/brew
 
@@ -72,10 +81,29 @@ brew install\
 	chxcode\
 	heroku
 
+echo "\n\n"
+
+echo "=================="
+echo "Installing yarn..."
+echo "=================="
 curl -o- -L https://yarnpkg.com/install.sh | bash
+
+echo "\n\n"
+
+echo "================="
+echo "Installing rvm..."
+echo "================="
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash
+
+echo "\n\n"
+
+echo "================="
+echo "Installing gvm..."
+echo "================="
 \curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer
 
-echo "Run the following afterwards:"
-echo "open /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app"
+echo "\n\n"
+
+echo "Run the following commands afterwards:"
+echo "$ open /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app"
