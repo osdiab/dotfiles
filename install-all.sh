4e5c2333d2aad024fd51ef6425234f6baba3a573
Omar Diab <me@omardiab.com>
2020-07-27 12:59:12 +0900

    install all script

---
+++ install-all.sh
@@ -0,0 +1,35 @@
+(
+  SCRIPT=$(realpath "$0")
+  # Absolute path this script is in, thus /home/user/bin
+  SCRIPTPATH=$(dirname "$SCRIPT")
+
+  echo "~~~~~~~~~~~~~~~~~~~"
+  echo "Installing packages"
+  echo "~~~~~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/install.sh"
+  echo "/n/n"
+
+  echo "~~~~~~~~~~~~~~~~~~~"
+  echo "Installing zsh deps"
+  echo "~~~~~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/zsh/install.sh"
+  echo "/n/n"
+
+  echo "~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "Installing kitty config"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/kitty/install.sh"
+  echo "/n/n"
+
+  echo "~~~~~~~~~~~~~~~~~~~~~~"
+  echo "Installing nvim config"
+  echo "~~~~~~~~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/vim/install.sh"
+  echo "/n/n"
+
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "Installing vscode config"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/vscode/install.sh"
+  echo "/n/n"
+)
