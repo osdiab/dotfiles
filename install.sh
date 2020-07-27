3db1c898d6afdbf8ec8d7698b2a7c34bb01c0f48
Omar Diab <me@omardiab.com>
2020-07-27 12:46:34 +0900

    update installed packages

--- install.sh
+++ install.sh
@@ -3,17 +3,63 @@
 
 # install casks
 
-brew cask install docker kitty visual-studio-code little-snitch google-chrome bartender firefox alfred loom native-access rekordbox 1password
+brew cask install\
+	1password\
+	disk-inventory-x\
+	keka\
+	obs\
+	steam\
+	alfred\
+	divvy\
+	keybase\
+	rekordbox\
+	teamviewer\
+	anki\
+	docker\
+	kindle\
+	signal\
+	the-unarchiver\
+	backblaze\
+	dupeguru\
+	kitty\
+	sketch\
+	transmission\
+	bartender\
+	firefox\
+	krisp\
+	skype\
+	visual-studio-code\
+	battle-net\
+	font-input\
+	little-snitch\
+	slack\
+	vlc\
+	cyberghost-vpn\
+	google-chrome\
+	loom\
+	soundflower\
+	xld\
+	deepl\
+	handbrake\
+	minecraft\
+	soundflowerbed\
+	zulip\
+	discord\
+	imageoptim\
+	native-access\
+	spotify
 
 # install cli tools
 brew tap klaaspieter/formula
+brew tap heroku/brew
 
 brew install\
 	hub\
 	httpie\
 	ripgrep\
 	nvim\
-	coreutils \
+	postgres\
+	coreutils\
 	antigen\
 	pyenv\
 	pyenv-virtualenv\
@@ -23,9 +69,13 @@ brew install\
 	httpie\
 	jq\
 	jabba\
-	chxcode
+	chxcode\
+	heroku
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash
 \curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer
+
+echo "Run the following afterwards:"
+echo "open /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app"
