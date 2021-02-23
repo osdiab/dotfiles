5e58d3e46ce9556e8dbdac0f930baea4645b0587
Omar Diab <me@omardiab.com>
2021-02-23 16:51:48 +0900

    Colemak keyboard stuff

---
+++ keyboard.sh
@@ -0,0 +1,5 @@
+(
+  git clone git@github.com:ColemakMods/mod-dh.git "$HOME/code/open-source/colemak-mod-dh"
+  sudo cp -r "$HOME/code/open-source/colemak-mod-dh/OS_X/ColemakDH-ansi.bundle" "/Library/Keyboard Layouts"
+  echo "Please open System Preferences > Keyboard > Input Sources and set source"
+)
