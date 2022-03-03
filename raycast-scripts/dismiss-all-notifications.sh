6e921b54875b7c6bf6383e6563338b46ad1e9926
Omar Diab <me@omardiab.com>
2022-03-03 16:35:50 +0900

    add raycast scripts

---
+++ raycast-scripts/dismiss-all-notifications.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Dismiss All Notifications
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Mac OSX
+
+open ~/Library/Mobile\ Documents/com~apple~Automator/Documents/Dismiss\ All\ Notifications.app
